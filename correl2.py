db49caef53d899affea3e54be7945fe148b693c2
Matus M <matuscogscimarton@gmail.com>
2019-06-18 19:03:18 +0000

    spark-submit

--- correl2.py
+++ correl2.py
@@ -1,5 +1,5 @@
 from pyspark.sql import SparkSession
-from pyspark.ml.stat import Correlation
+import pandas as pd
 
 spark = SparkSession\
         .builder\
@@ -13,9 +13,18 @@ rawData = spark.sql("select * from googleplay")
 df_gp = rawData.toPandas()
 a=df_gp.corr()
 a.to_csv("a.csv")
-s = open("a.csv").read()
-t=open("a.csv",'w')
-t.write("x")
-t.write(s)
-t.close
+
+#with open('a.csv','r') as f:
+#    s = f.read()
+#s = open("a.csv").read()
+#t=open("a.csv",'w')
+#t.write("x")
+#t.write(s)
+#t.close
+pd.read_csv("a.csv")
+df_corr = pd.read_csv("a.csv")
+df_target = spark.createDataFrame(df_corr.astype(str)) 
+df_target.createOrReplaceTempView("temp_task5")
+spark.sql("create table task as select * from temp_task5");
+
 spark.stop()
